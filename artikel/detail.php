11644e6bbd41fa8b43e3673790a5b19d14e7a368
Nadia AS <aulinanadia21@gmail.com>
2026-06-09 13:51:52 +0700

    Delete artikel directory

--- artikel/detail.php
+++
@@ -1,8 +0,0 @@
-<?= $this->include('template/header'); ?>
-
-<article class="entry">
-    <h2><?= $artikel['judul']; ?></h2>
-    <p><?= $artikel['isi']; ?></p>
-</article>
-
-<?= $this->include('template/footer'); ?>
